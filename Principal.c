601d6ea063c5048d3e8b4ea675ce06d53c7b128d
Julio Francisco Motta Claudio <59737000+FranciscoMotta@users.noreply.github.com>
2020-06-12 11:04:35 -0500

    Se añade comentarios

--- Principal.c
+++ Principal.c
@@ -13,9 +13,13 @@
 
 #define _XTAL_FREQ 20000000UL
 
+//DEFINICION DE LOS PINES DE CONTROL DE LA MATRIZ
+
 #define DATA_OUT LATCbits.LATC0
 #define CLOCK_OUT LATCbits.LATC1
 #define PUERTOS_OUT LATB
+
+#DEFINICION DE LAS FUNCIONES DE TRABAJO
 
 void pinesInicio(void);
 void pulsoReloj(void);
@@ -23,25 +27,33 @@ void talkMatrix(void);
 void notTalk(void);
 void Escribir_Valor_En_Lcd(uint16_t valor);
 
+//Principal
+
 void main(void) {
-    Iniciar_Lcd();
-    pinesInicio();
-    Lcd_Escribir_Cadena("HOLA");
-    ADC_inicializacion(ADC_ON);
-    char var = 0x00;
-    int datoDeCuentaXD = 0;
+    Iniciar_Lcd(); //Iniciamos el LCD para ver los datos procedentes del microfono
+    pinesInicio(); //Iniciamos los pines de trabajo
+    Lcd_Escribir_Cadena("HOLA"); // Mensaje de prueba para comprobar el funcionamiento del LCD
+    ADC_inicializacion(ADC_ON); //Activamos el conversos an谩logo digital del pic18f4550
+    char var = 0x00; //Creamos una variable del tipo caracter para almacenar los datos 
+    int datoDeCuentaXD = 0; //Creamos otra variable para que sirva de conteo para las repeticiones
 PASO:
-    Posicion_Linea_Superior(0);
-    var = Leer_ADC(CANAL_AN0);
-    Escribir_Valor_En_Lcd(var);
-    if (var <= 43) {
-        notTalk();
-    } else {
-        for (datoDeCuentaXD = 0; datoDeCuentaXD < 80; datoDeCuentaXD++) {
-            talkMatrix();
+    Posicion_Linea_Superior(0); //Nos dirigimos a la primer linea del LCD
+    var = Leer_ADC(CANAL_AN0); //En var guardamos la lectura del canal AN0 donde estar铆a el microfono 
+     /*PARA ESTAR PARTE ES IMPORTANTE REVISAR EL PROTEUS ADJUNTO PARA VER EL SISTEMA DE AMPLIFICACI脫N*/
+    Escribir_Valor_En_Lcd(var); //Mostramos el valor de voltaje producido por el microfono al hablar o hacer silencio
+      /* NOTAS:
+       * Esta parte depende mucho del microfono que uses o del sistema de amplificaci贸n que emplees para obtener el voltaje del microfono
+       * Como se a帽adi贸 el LCD , tu puedes ver all铆 el valor que se registra al hablar o al hacer silencio y poner ello en las condiciones de 
+       * funcionamiento
+      */
+    if (var <= 43) { //En este caso, el microfono registra 0.43 volts cuando hay silencio, osea que a partir de ese voltaje se considera que hablamos
+        notTalk(); //Si el dato es menor, mostramos la configuraci贸n en el LCD que aparenta estar en silencio
+    } else { // De lo contrario, mostramos el movimiento de los labios
+        for (datoDeCuentaXD = 0; datoDeCuentaXD < 80; datoDeCuentaXD++) { //Estas son las repeticiones para apreciar mejor el movimiento de los labios
+            talkMatrix(); //Mostramos el movimiento
         }
     }
-    goto PASO;
+    goto PASO; //Regresamos a hacer todo otra vez
     return;
 }
 
@@ -65,8 +77,8 @@ void Escribir_Valor_En_Lcd(uint16_t valor) {
 
     /*FRACCIONAMIENTO EN DECENAS, UNIDADES Y CENTENAS*/
     Centenas_Del_Valor_Introducido = valor / 100; /*Dividimos en 100 para quedarnos con solo las centenas como parte entera*/
-    Decenas_Del_Valor_Introducido = (valor % 100) / 10; /*Sac醡os el m骴ulo 100 al valor introducido y luego lo dividimos entre 10 para quedanos con las decenas como parte entera*/
-    Unidades_Del_Valor_Introducido = (valor % 100) % 10; /*Sac醡os el m骴ulo 100 al valor introducido y sacamos el m骴ulo 10 para quedanos con las unidades como parte entera*/
+    Decenas_Del_Valor_Introducido = (valor % 100) / 10; /*Sac谩mos el m贸dulo 100 al valor introducido y luego lo dividimos entre 10 para quedanos con las decenas como parte entera*/
+    Unidades_Del_Valor_Introducido = (valor % 100) % 10; /*Sac谩mos el m贸dulo 100 al valor introducido y sacamos el m贸dulo 10 para quedanos con las unidades como parte entera*/
 
     Lcd_Escribir_Caracter(Centenas_Del_Valor_Introducido + 0x30); /*Se muestra el dato correspondiente y se le adiciona 0X30 para convertirlo a ASCII*/
     Lcd_Escribir_Caracter('.');
